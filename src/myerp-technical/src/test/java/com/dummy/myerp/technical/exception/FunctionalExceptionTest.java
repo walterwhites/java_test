00e8ba72a342d593509732c05cca41f819cbab14
walterwhites <hopemagie@gmail.com>
2019-09-29 00:13:27 +0200

    technical

---
+++ src/myerp-technical/src/test/java/com/dummy/myerp/technical/exception/FunctionalExceptionTest.java
@@ -0,0 +1,36 @@
+package com.dummy.myerp.technical.exception;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FunctionalExceptionTest extends Exception {
+
+    @Test
+    public void functionnalExceptionWithMessage() {
+        FunctionalException functionalException = assertThrows(FunctionalException.class, () -> {
+            throw new FunctionalException("mon message fonctionnel");
+        });
+        assertEquals("mon message fonctionnel", functionalException.getMessage());
+    }
+
+    @Test
+    public void functionnalExceptionWithCause() {
+        FunctionalException functionalException = assertThrows(FunctionalException.class, () -> {
+            Throwable throwable = new Throwable();
+            throw new FunctionalException(throwable);
+        });
+        assertEquals(Throwable.class, functionalException.getCause().getClass());
+    }
+
+    @Test
+    public void functionnalExceptionWithMessageAndCause() {
+        FunctionalException functionalException = assertThrows(FunctionalException.class, () -> {
+            Throwable throwable = new Throwable();
+            throw new FunctionalException("mon message fonctionnel 2", throwable);
+        });
+        assertEquals(Throwable.class, functionalException.getCause().getClass());
+        assertEquals("mon message fonctionnel 2", functionalException.getMessage());
+    }
+}
